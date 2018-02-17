77904bcce044746e611d2f383dd6a39f63ca11f9
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2018-02-17 02:00:47 +0100

    initial commit off everything I did until now

---
+++ config.py
@@ -0,0 +1,9 @@
+flickr_apikey = 'your flickr api key'
+
+# application id & secret in https://www.reddit.com/prefs/apps
+reddit_clientid = 'your reddit app client id'
+reddit_clientsecret = 'your reddit app client secret'
+# user agent should be <platform>:<app ID>:<version string> (by /u/<Reddit username>)
+reddit_useragent = 'your app user agent'
+reddit_username = 'your username'
+reddit_password = 'password for username'
